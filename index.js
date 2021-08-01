3f480626f042b5d35ea080fb180482c552359316
vanflux <lucasabruzzi@hotmail.com>
2021-08-01 12:10:48 -0300

    Nodemon dep & index exports

---
+++ index.js
@@ -0,0 +1,8 @@
+// Export for npm module
+
+module.exports = {
+  Client: require('./src/client/client'),
+  Server: require('./src/client/server'),
+  ServerList: require('./src/client/serverList'),
+  Character: require('./src/client/character'),
+};
